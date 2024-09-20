7cdd39b0a59ff95264ffb49c4b369c446854f1d2
Aero327 <vl.konovalov59@gmail.com>
2024-09-20 14:24:02 +0500

    Решил задачи 1.6 и 1.8
    TODO: добваить проверки, где они нужны

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ internal class Program {
         System.Console.WriteLine("Сумма двух последних знаков числа: " + result1);
         */
         // Задание 1.4
+        /*
         int x2; 
         bool result2;
 
@@ -25,6 +26,41 @@ internal class Program {
         result2 = pr.isPositive(x2);
         if (result2) System.Console.WriteLine("Число положительное");
         else System.Console.WriteLine("Число не положительное");
+        */
+        // Задание 1.6
+        /*
+        char x3; 
+        bool result3;
+
+        System.Console.Write("Введите символ: ");
+        x3 = Convert.ToChar(System.Console.ReadLine());
+
+        result3 = pr.isUpperCase(x3);
+        if (result3) System.Console.WriteLine("Символ является прописной буквой");
+        else System.Console.WriteLine("Символ не является прописной буквой");
+        */
+        // Задание 1.8
+        int a4, b4;
+        bool result;
+
+        System.Console.Write("Введите число a: ");
+        a4 = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.Write("Введите число b: ");
+        b4 = Convert.ToInt32(Console.ReadLine());
+
+        if ((a4 == 0 || b4 == 0) && !(a4 == 0 && b4 == 0)) 
+            System.Console.WriteLine("Одно из чисел делит другое нацело");
+
+        else if (a4 == 0 && b4 == 0)
+            System.Console.WriteLine("Ошибка: деление на 0");
+
+        else {
+            result = pr.isDivisor(a4, b4);
+            if (result) System.Console.WriteLine("Одно из чисел делит другое нацело");
+            else System.Console.WriteLine("Ни одно из чисел не делит другое нацело");
+        }
+
     }
 
     // Задание 1.2
@@ -34,7 +70,18 @@ internal class Program {
         return (num[num.Length - 1] - '0') + (num[num.Length - 2] - '0');
     }
 
+    // Задание 1.4
     public bool isPositive(int x) {
         return x > 0;
+    }
+
+    // Задание 1.6
+    public bool isUpperCase(char x) {
+        return char.IsUpper(x);
+    }
+
+    // Задание 1.8
+    public bool isDivisor(int a, int b) {
+        return a % b == 0 || b % a == 0;
     }
 }
